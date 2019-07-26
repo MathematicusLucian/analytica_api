9add17ca23739bc573b0f3ee1d2d5868b2cb3442
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-07-26 02:38:19 +0100

    .htaccess, and mp call, etc

--- db-login.php
+++ db-login.php
@@ -3,13 +3,20 @@
 
     include './config/pw.php'; 
 
+    define('DB_HOST', $server_name);
+    define('DB_USER', $user_name);
+    define('DB_PASS', $pw);
+    define('DB_NAME', $db_name);
+
     // Create connection
-    $conn = new mysqli($server_name, $user_name, $pw, $db_name);
+    $conn = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     } else {
         //echo "Connected";
     }
+
+    mysqli_set_charset($connect, "utf8");
 
 ?> 
--- getTwoRandomMPs.php
+++ getTwoRandomMPs.php
@@ -1,5 +1,27 @@
 <?php
     include 'db-login.php'; 
 
-    echo '{name: "test"}'; 
+    $mps = [];
+
+    $sql = "SELECT `mp_name`,` constit`,`rating`,` mp_img` FROM `mps` WHERE 1";
+
+    $result = $conn->query($sql);
+    
+    if ($result->num_rows > 0) {
+        $i = 0;
+        while($row = $result->fetch_assoc()) {
+            $mps[$i]["name"] = $row["mp_name"];
+            $mps[$i]["constit"] = $row[" constit"];
+            $mps[$i]["rating"] = $row["rating"];
+            $mps[$i]["img"] = $row[" mp_img"];
+            $i++;
+        }
+        echo json_encode(['data'=>$mps]);
+    } else {
+        //echo " - 0 results";
+        http_response_code(404);
+    }
+    
+    $conn->close();
+
 ?>
