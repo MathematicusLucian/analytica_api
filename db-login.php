efe3217a6e1a3c1f98fcd2120fb8ad3b0483cda0
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-07-26 19:45:41 +0100

    Access-Control-Allow-Headers

--- db-login.php
+++ db-login.php
@@ -1,5 +1,8 @@
 <?php
     header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Credentials: true"');
+    header('Access-Control-Allow-Methods: POST, PUT, GET, OPTIONS');
+    header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
     include './config/pw.php'; 
 
