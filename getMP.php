a081b67331d4093095160dcaeb00f422b2113b41
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-07-26 20:45:31 +0100

    pass id on get two random mps

--- getMP.php
+++ getMP.php
@@ -10,11 +10,12 @@
     if ($result->num_rows > 0) {
         $i = 0;
         while($row = $result->fetch_assoc()) {
+            $mps[$i]["id"] = $row["code_press"];
             $mps[$i]["name"] = $row["mp_name"];
+            $mps[$i]["constit"] = $row["constit"];
             $mps[$i]["img"] = $row["mp_img"];
             $mps[$i]["gender"] = "1";
             $mps[$i]["rating"] = $row["rating"];
-            //$mps[$i]["constit"] = $row["constit"];
             $i++;
         }
         echo json_encode(['data'=>$mps]);
--- getTwoRandomMPs.php
+++ getTwoRandomMPs.php
@@ -4,20 +4,20 @@
     $gender =  $_GET["g"]; 
 
     $mps = [];  
-
-    //need filter by gender - and random
-    $sql = "SELECT `mp_name`,`constit`,`rating`,`gender`,`mp_img` FROM `mps` WHERE `gender` = " . $gender . " ORDER BY RAND() LIMIT 2";
+ 
+    $sql = "SELECT `code_press`,`mp_name`,`constit`,`rating`,`gender`,`mp_img` FROM `mps` WHERE `gender` = " . $gender . " ORDER BY RAND() LIMIT 2";
 
     $result = $conn->query($sql);
     
     if ($result->num_rows > 0) {
         $i = 0;
         while($row = $result->fetch_assoc()) {
+            $mps[$i]["id"] = $row["code_press"];
             $mps[$i]["name"] = $row["mp_name"];
+            $mps[$i]["constit"] = $row["constit"];
             $mps[$i]["img"] = $row["mp_img"];
             $mps[$i]["gender"] = $row["gender"];
             $mps[$i]["rating"] = $row["rating"];
-            //$mps[$i]["constit"] = $row["constit"];
             $i++;
         }
         echo json_encode(['data'=>$mps]);
